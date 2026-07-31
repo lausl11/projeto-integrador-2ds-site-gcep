2e5041f097cc513efb8cd743ae767ab199a5fa4e
lausl11 <laura.laurindo@escola.pr.gov.br>
2026-07-31 09:34:53 -0300

    criei a conexão servidor node.js - server.js

---
+++ scr/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+// Configuração do Handlebars
+app.engine('handlebars', engine({
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, '../views/layouts'),
+  partialsDir: path.join(__dirname, '../views/partials')
+}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '../views'));
+
+// Arquivos estáticos (CSS, JS, imagens)
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Para receber dados de formulários
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Rota de teste (página inicial)
+app.get('/', (req, res) => {
+  res.render('home', {
+    titulo: 'GCEP - Página Inicial'
+  });
+});
+
+
+module.exports = app;
---
+++ scr/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express ()
+
+app.listen(3000, function () {
+    console.log('servidor rodando em url: localhost:300')
+});
