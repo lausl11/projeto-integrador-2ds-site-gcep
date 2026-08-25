065da6a0f8f7c41f1eeda3a24159e56704b2af95
lau.sl11 <laura.laurindo@Escola.pr.gov.br>
2026-08-25 13:14:01 -0300

    atualização das rotas 01

--- scr/app.js
+++ scr/app.js
@@ -4,4 +4,12 @@ const app = express();
 
 
 
+
+
+
+app.post('/login', function(req, res) {
+    res.sendFile(path.join(__dirname, '../pages/login.html'));
+});
+
+app.use(express.static(path.join(__dirname, '../public')));
 module.exports = app;
--- scr/routes/login.routes.js
+++
@@ -1,9 +0,0 @@
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/login.html'));
-});
-
-App.post('/login', function(req, res){
-    res.sendFile(path.join(__dirname, '../public/login.html'))
-})
-
-module.exports = app;
